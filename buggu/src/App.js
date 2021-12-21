b37843fe273a1cea4e30905bba3673ba19f272cb
ajaynamdev1 <ajaynamdev701@gmail.com>
2021-12-21 20:42:29 +0530

    first commit

---
+++ buggu/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CovidData from './components/CovidData';
+
+
+function App() {
+  return (
+    <div className="App">
+     <CovidData/>
+    </div>
+  );
+}
+
+export default App;
